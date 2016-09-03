1214c593415b76d634691363a4c32b5de2461751
adndebanane <adndebanane@adndebanane.net>
2016-09-03 20:07:47 +0200

    script to copy paste in zdz message board

---
+++ zds_print.R
@@ -0,0 +1,8 @@
+library(knitr)
+calendar <- read.table("00-datasets/20162017/calendar_l1_1617.csv", header = T,  
+                       stringsAsFactors = F, sep = ",")
+load("results20162017.rda")
+cur.week <- nrow(season20162017)/10 + 1
+df2print <- cbind(calendar[calendar$Week == cur.week, ], rep("pseudo", 10))
+names(df2print) <- c("Domicile", "Visiteur", "Week", "Pseudo")
+kable(df2print[, -3], format = "markdown", row.names = F)
