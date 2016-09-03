75be319eebf5c95730e3192c5dd050303d97c0ed
adndebanane <adndebanane@adndebanane.net>
2016-09-03 21:35:17 +0200

    set new name for input of player bets

--- ranking_players.R
+++ ranking_players.R
@@ -1,37 +1,47 @@
 load("results20162017.rda")
 cur.season <- season20162017
-bets <- read.table(file = "00-datasets/20162017/bets.csv", header = TRUE, sep = ";", 
+bets <- read.table(file = "00-datasets/20162017/players_bets.csv", header = TRUE, sep = ",", 
                    stringsAsFactors = F)
 pot.pergame <- 10 #euros
+eta <- 1/8
 
-name.players <- unique(bets$expert);
-nb.players <- length(name.experts);
+name.players <- unique(bets$player)
+nb.players <- length(name.players)
 players <- data.frame(id               = 1:nb.players, 
                       player           = name.players, 
                       profits          = rep(0, nb.players),
+                      weights          = rep(1, nb.players),
                       nb.wins          = rep(0, nb.players),
                       wins.ratio       = rep(0, nb.players),
                       nb.match.played  = rep(0, nb.players), 
                       stringsAsFactors = FALSE)
 nb.matchs <- nrow(cur.season)
+max.played <- max(table(bets$player))
 
-for (iplayer in 1:nb.experts){
-  played.games <- bets$idmatch[bets$expert == players$player[iplayer]]
+
+for (iplayer in 1:nb.players){
+  played.games <- bets$idmatch[bets$player == players$player[iplayer]]
   games.idres <- match(played.games, cur.season$idmatch)
-  cur.results <- cur.season$results[games.idres]
+  cur.results <- cur.season$FTRfrench[games.idres]
   odds <- (cur.results == "1") * cur.season$B365H[games.idres] + 
           (cur.results == "N") * cur.season$B365D[games.idres] + 
           (cur.results == "2") * cur.season$B365A[games.idres] 
-  cur.wins <- bets$bet[bets$expert == players$player[iplayer]] == cur.season$results[games.idres]
+  cur.wins <- bets$bet[bets$player == players$player[iplayer]] == cur.season$FTRfrench[games.idres]
   cur.profits <- sum(pot.pergame * odds * cur.wins) - sum(pot.pergame * (1/odds) * !cur.wins )
-  players$profits[iplayer] <- players$profits[iplayer] + cur.profits
+  nb.played <- length(played.games)
+  nb.won <- sum(cur.wins)
+  players$profits[iplayer] <- cur.profits
   players$nb.match.played[iplayer] <- length(played.games)
   players$nb.wins[iplayer] <- sum(cur.wins)
-  players$wins.ratio[iplayer] <- sum(cur.wins) / length(played.games)
+  players$wins.ratio[iplayer] <- nb.won / nb.played
+  players$weights[iplayer] <- players$weights[iplayer] * (1 - eta)**(nb.played - 
+                              nb.won + 2/3*(max.played - nb.played))
 }
 players
 players$profits / sqrt(players$nb.match.played)
 save(players, file = "players.rda")
+write.table(players, quote = FALSE, sep = ",", row.names = FALSE,
+            file = "00-datasets/20162017/players_rankings.csv")
 
 profits.pergame <- (players$profits/players$nb.match.played)
 profits.pergame / max(abs(profits.pergame))
